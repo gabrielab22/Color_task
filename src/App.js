e9e4806fa7c860be83bd36678b5a9469f7ce0be2
gabrielab22 <gabriela.bilanzic@gmail.com>
2021-06-18 20:16:04 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from "react";
+import styled from 'styled-components';
+import { Button } from 'react-bootstrap';
 
-function App() {
+
+
+
+
+const App = () => {
+
+  const [colorList, setColorList] = useState([
+    "66504c",
+    "dcd8d7",
+    "008000"
+  ]);
+
+  const [activeColor, setActiveColor] = useState(0);
+
+
+  const onGetRandomColorHandler = () => {
+
+    setColorList([...colorList, "800000"])
+    setActiveColor(activeColor + 1);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <List>
+          {colorList.map((color, index) => <ListItem key={index} color={color} active />)}
+
+        </List>
+        <Button onClick={onGetRandomColorHandler} variant="primary" >Click to change color</Button>{' '}
       </header>
     </div>
   );
 }
+
+const List = ({ children }) => {
+
+  return (
+    <div >{children}</div>
+  )
+};
+
+const ListItem = ({ color, active }) => {
+
+  return (
+    <div style={{ color: "#" + color }}>{color}</div>
+  )
+};
+
+// const styles = {
+//   container: {
+//     display: "block",
+//     justifyContent: "center",
+//     width: "300px",
+//     height: "300px",
+//     backgroundColor: "#44ddff"
+//   }
+// }
 
 export default App;
