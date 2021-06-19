6b5bc022484c503e010bd3379d2b9d3291d3b16f
gabrielab22 <gabriela.bilanzic@gmail.com>
2021-06-19 23:04:45 +0200

    third commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import './App.css';
 import React, { useState } from "react";
-import styled from 'styled-components';
 import { Button } from 'react-bootstrap';
+import styled from 'styled-components';
+
+const FetchBtn = styled.button`
+  background-color: ${({ activeColor }) => "#" + activeColor};
+  display: block;
+  width: auto;
+  height: auto;
+  padding: 5px;
+  border-radius: 5px;
+`;
 
 
 const App = () => {
@@ -18,24 +27,46 @@ const App = () => {
     })
       .then(res => res.json())
       .then(data => {
-        console.log(data.new_color)
-        setColorList([...colorList, data.new_color])
-        setActiveColor(activeColor + 1);
+        if (!colorList.includes(data.new_color)) {
+          setColorList([...colorList, data.new_color])
+          setActiveColor(activeColor && activeColor !== colorList.length - 1 ? colorList.length : activeColor + 1)
+        };
       });
   }
 
+  const upBtn = () => {
+    if (activeColor > 0) {
+      setActiveColor(activeColor - 1)
+    }
+  }
 
+  const downBtn = () => {
+    if (activeColor < colorList.length - 1) {
+      setActiveColor(activeColor + 1)
+    }
+  }
 
   return (
     <div className="App">
       <header className="App-header">
+        <FetchBtn
+          onClick={onGetRandomColorHandler}
+          activeColor={colorList[activeColor]}
+        >
+          {colorList.length ? colorList[activeColor] : 'Click me'}
+        </FetchBtn>
+        <Button onClick={upBtn} variant="secondary" size="sm">Up</Button>
+        <Button onClick={downBtn} variant="secondary" size="sm">Down</Button>
         <List>
-          {colorList.map((color, index) => <ListItem class="active" key={index} color={color} />)}
+          {colorList.filter(String).map((color, index) =>
+            <ListItem
+              key={index}
+              color={color}
+              setActiveColor={() => setActiveColor(index)}
+              className={activeColor === index ? 'active-color' : ''} />
+          )}
 
         </List>
-        <Button onClick={onGetRandomColorHandler} variant="primary" size="lg"> Click</Button>{' '}
-        <Button variant="secondary" size="sm">Up</Button>{' '}
-        <Button variant="secondary" size="sm">Down</Button>{' '}
 
       </header>
     </div >
@@ -45,25 +76,29 @@ const App = () => {
 const List = ({ children }) => {
 
   return (
-    <div >{children}</div>
+    <div style={{ display: "flex", flexDirection: "column" }}>{children}</div>
   )
 };
 
-const ListItem = ({ color, active }) => {
-
+const ListItem = ({ color, className, setActiveColor }) => {
   return (
-    <div style={{ color: "#" + color }}>{color}</div>
+    <div
+      style={{
+        color: "black",
+        backgroundColor: "#" + color,
+        display: "block",
+        width: "auto",
+        height: "auto",
+        padding: "5px",
+        borderRadius: "5px",
+        marginTop: "1rem"
+      }}
+      className={className}
+      onClick={setActiveColor}
+    >
+      {color}
+    </div>
   )
 };
-
-// const styles = {
-//   container: {
-//     display: "block",
-//     justifyContent: "center",
-//     width: "300px",
-//     height: "300px",
-//     backgroundColor: "#44ddff"
-//   }
-// }
 
 export default App;
